d761f5134db10a4edc2ac2576f2c2245b26aaafc
Eduardo Rodrigues <mrodrigues.eduardo@gmail.com>
2019-07-02 21:57:07 -0300

    Cria pasta de documentos de atualização da aula 10.13 para o Angular 8

---
+++ 10.13-docs-para-atualizacao-angular-8/pessoas-pesquisa.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pessoas-pesquisa',
+  templateUrl: './pessoas-pesquisa.component.html',
+  styleUrls: ['./pessoas-pesquisa.component.css']
+})
+export class PessoasPesquisaComponent {
+  pessoas = [
+    { nome: 'Manoel Pinheiro', cidade: 'Uberlândia', estado: 'MG', ativo: true },
+    { nome: 'Sebastião da Silva', cidade: 'São Paulo', estado: 'SP', ativo: false },
+    { nome: 'Carla Souza', cidade: 'Florianópolis', estado: 'SC', ativo: true },
+    { nome: 'Luís Pereira', cidade: 'Curitiba', estado: 'PR', ativo: true },
+    { nome: 'Vilmar Andrade', cidade: 'Rio de Janeiro', estado: 'RJ', ativo: false },
+    { nome: 'Paula Maria', cidade: 'Uberlândia', estado: 'MG', ativo: true }
+  ];
+}
