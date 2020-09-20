a9310be5bdc34180e3d8a7a6de50be982f059dfa
Fagner Lima <contato@fagnerlima.pro.br>
2020-09-20 10:22:26 -0300

    Atualiza aula 9.16 para o ng10

---
+++ 9.16-adicionando-estilos-css-em-componentes-ng10/src/app/funcionario-card/funcionario-card.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-funcionario-card',
+  templateUrl: './funcionario-card.component.html',
+  //styleUrls: ['./funcionario-card.component.css']
+  styles: [`
+    .card-block {
+      text-transform: uppercase;
+      color: blue;
+    }
+  `]
+})
+export class FuncionarioCardComponent {
+
+  @Input() funcionario: any;
+
+}
