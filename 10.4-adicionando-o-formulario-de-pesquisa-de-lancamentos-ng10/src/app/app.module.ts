0deac858016f7c8a9758833e322a1cfccdfae828
Fagner Lima <contato@fagnerlima.pro.br>
2020-09-22 09:16:05 -0300

    Atualiza aula 10.4 para o ng10

--- 10.4-adicionando-o-formulario-de-pesquisa-de-lancamentos-ng10/src/app/app.module.ts
+++ 10.4-adicionando-o-formulario-de-pesquisa-de-lancamentos-ng10/src/app/app.module.ts
@@ -2,8 +2,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { InputTextModule } from 'primeng/components/inputtext/inputtext';
-import { ButtonModule } from 'primeng/components/button/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
 
 import { AppComponent } from './app.component';
 
