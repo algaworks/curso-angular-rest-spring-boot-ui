24cec2a5deb199ba4779c3da803653340be0ade7
Fagner Lima <contato@fagnerlima.pro.br>
2020-09-23 09:12:08 -0300

    Atualiza aula 10.6 para o ng10

---
+++ 10.6-customizando-colunas-com-ng-template-ng10/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    InputTextModule,
+    ButtonModule,
+    TableModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
