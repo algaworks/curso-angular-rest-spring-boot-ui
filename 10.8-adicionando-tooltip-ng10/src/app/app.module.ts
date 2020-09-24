751deb848e7a22f18058bd83012c3fc5b6d7a465
Fagner Lima <contato@fagnerlima.pro.br>
2020-09-24 14:41:50 -0300

    Atualiza a aula 10.8 para o ng10

--- 10.8-adicionando-tooltip-ng10/src/app/app.module.ts
+++ 10.8-adicionando-tooltip-ng10/src/app/app.module.ts
@@ -2,10 +2,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { InputTextModule } from 'primeng/components/inputtext/inputtext';
-import { ButtonModule } from 'primeng/components/button/button';
-import { DataTableModule } from 'primeng/components/datatable/datatable';
-import { TooltipModule } from 'primeng/components/tooltip/tooltip';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
+import { TooltipModule } from 'primeng/tooltip';
 
 import { AppComponent } from './app.component';
 
@@ -17,7 +17,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     InputTextModule,
     ButtonModule,
-    DataTableModule,
+    TableModule,
     TooltipModule
   ],
   providers: [],
