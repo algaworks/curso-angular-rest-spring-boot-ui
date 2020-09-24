8ba49ff9f130f6e3ab90894fb0768f2cf0454f62
Fagner Lima <contato@fagnerlima.pro.br>
2020-09-24 13:48:13 -0300

    Inicia a atualização da aula 10.7

---
+++ 10.7-fazendo-paginacao-de-dados-ng10/src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  lancamentos = [
+    { tipo: 'DESPESA', descricao: 'Compra de pão', dataVencimento: '30/06/2017',
+      dataPagamento: null, valor: 4.55, pessoa: 'Padaria do José' },
+    { tipo: 'RECEITA', descricao: 'Venda de software', dataVencimento: '10/06/2017',
+      dataPagamento: '09/06/2017', valor: 80000, pessoa: 'Atacado Brasil' },
+    { tipo: 'DESPESA', descricao: 'Impostos', dataVencimento: '20/07/2017',
+      dataPagamento: null, valor: 14312, pessoa: 'Ministério da Fazenda' },
+    { tipo: 'DESPESA', descricao: 'Mensalidade de escola', dataVencimento: '05/06/2017',
+      dataPagamento: '30/05/2017', valor: 800, pessoa: 'Escola Abelha Rainha' },
+    { tipo: 'RECEITA', descricao: 'Venda de carro', dataVencimento: '18/08/2017',
+      dataPagamento: null, valor: 55000, pessoa: 'Sebastião Souza' },
+    { tipo: 'DESPESA', descricao: 'Aluguel', dataVencimento: '10/07/2017',
+      dataPagamento: '09/07/2017', valor: 1750, pessoa: 'Casa Nova Imóveis' },
+    { tipo: 'DESPESA', descricao: 'Mensalidade musculação', dataVencimento: '13/07/2017',
+      dataPagamento: null, valor: 180, pessoa: 'Academia Top' }
+  ];
+}
