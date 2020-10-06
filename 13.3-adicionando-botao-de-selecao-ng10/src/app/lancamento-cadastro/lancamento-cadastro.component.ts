dadfc08c7684ddd8c4476932a20990274201ebf1
Fagner Lima <contato@fagnerlima.pro.br>
2020-10-06 08:16:47 -0300

    Atualiza aula 13.3 para o ng10

---
+++ 13.3-adicionando-botao-de-selecao-ng10/src/app/lancamento-cadastro/lancamento-cadastro.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lancamento-cadastro',
+  templateUrl: './lancamento-cadastro.component.html',
+  styleUrls: ['./lancamento-cadastro.component.css']
+})
+export class LancamentoCadastroComponent implements OnInit {
+
+  tipos = [
+    { label: 'Receita', value: 'RECEITA' },
+    { label: 'Despesa', value: 'DESPESA' },
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
