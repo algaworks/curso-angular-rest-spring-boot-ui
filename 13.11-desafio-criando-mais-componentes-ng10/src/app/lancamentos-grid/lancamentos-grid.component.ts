4f315c1b3d4deef0d52d0ed98b20dc8d1aa40f50
Fagner Lima <contato@fagnerlima.pro.br>
2020-10-07 09:30:09 -0300

    Atualiza aula 13.11 para o ng10

---
+++ 13.11-desafio-criando-mais-componentes-ng10/src/app/lancamentos-grid/lancamentos-grid.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-lancamentos-grid',
+  templateUrl: './lancamentos-grid.component.html',
+  styleUrls: ['./lancamentos-grid.component.css']
+})
+export class LancamentosGridComponent {
+
+  @Input() lancamentos = [];
+
+}
