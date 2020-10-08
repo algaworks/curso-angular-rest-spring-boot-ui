ba07008cd72d40bc6f0dec949f869062253c910f
Fagner Lima <contato@fagnerlima.pro.br>
2020-10-08 15:03:40 -0300

    Atualiza aula 14.4 para o ng10

---
+++ 14.4-criando-um-componente-interno-do-modulo-ng10/src/app/navegacao/linha/linha.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-linha',
+  templateUrl: './linha.component.html',
+  styleUrls: ['./linha.component.css']
+})
+export class LinhaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
