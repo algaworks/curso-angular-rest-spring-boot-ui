926f2ba827ee2b7489a5e3fcf8ef10827c916d74
Fagner Lima <contato@fagnerlima.pro.br>
2020-10-08 18:48:52 -0300

    Atualiza a aula 14.11 para o ng10

---
+++ 14.11-desafio-criando-o-core-module-ng10/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { PessoasModule } from './pessoas/pessoas.module';
+import { LancamentosModule } from './lancamentos/lancamentos.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+
+    CoreModule,
+    LancamentosModule,
+    PessoasModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
