6fe11900bcb253ca7a33180f4c5466d6d0fee71c
Fagner Lima <contato@fagnerlima.pro.br>
2020-10-13 11:46:40 -0300

    Atualiza a aula 16.7 para o ng10

---
+++ 16.7-tratando-erros-de-requisicoes-http-ng10/src/app/cidade.service.ts
@@ -0,0 +1,33 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class CidadeService {
+
+  constructor(private http: HttpClient) { }
+
+  consultar(): Promise<any> {
+    return this.http.get('http://localhost:3000/cidades')
+      .toPromise();
+  }
+
+  adicionar(cidade: any): Promise<any> {
+    return this.http.post('http://localhost:3000/cidades', cidade)
+      .toPromise();
+  }
+
+  excluir(id: number): Promise<void> {
+    return this.http.delete(`http://localhost:3000/cidades/${id}`)
+      .toPromise()
+      .then(() => null);
+  }
+
+  atualizar(cidade: any): Promise<any> {
+    return this.http.put(`http://localhost:3000/cidades/${cidade.id}`, cidade)
+      .toPromise()
+      .catch(erro => {
+        return Promise.reject(`Erro ao alterar cidade ${cidade.id}.`);
+      });
+  }
+
+}
