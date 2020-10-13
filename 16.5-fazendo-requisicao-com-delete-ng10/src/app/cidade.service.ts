a8bcc81514cecc2254efe12eabb934db04071780
Fagner Lima <contato@fagnerlima.pro.br>
2020-10-13 11:35:55 -0300

    Atualiza a aula 16.5 para o ng10

---
+++ 16.5-fazendo-requisicao-com-delete-ng10/src/app/cidade.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class CidadeService {
+
+  constructor(private http: HttpClient) { }
+
+  consultar(): Promise<any> {
+    return this.http.get('http://localhost:3000/cidades')
+      .toPromise();
+  }
+
+  adicionar(cidade: any): Promise<any> {
+    return this.http.post('http://localhost:3000/cidades', cidade)
+      .toPromise();
+  }
+
+  excluir(id: number): Promise<void> {
+    return this.http.delete(`http://localhost:3000/cidades/${id}`)
+      .toPromise()
+      .then(() => null);
+  }
+
+}
