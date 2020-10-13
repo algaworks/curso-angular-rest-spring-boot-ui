638c659321cadc9e6996ccf0155555f94591f64b
Fagner Lima <contato@fagnerlima.pro.br>
2020-10-13 11:42:24 -0300

    Atualiza a aula 16.6 para o ng10

---
+++ 16.6-fazendo-requisicao-com-put-ng10/src/app/app.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+
+import { CidadeService } from './cidade.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+
+  cidades = [];
+
+  constructor(private cidadeService: CidadeService) {}
+
+  ngOnInit() {
+    this.consultar();
+  }
+
+  consultar() {
+    this.cidadeService.consultar()
+      .then(dados => {
+        this.cidades = dados;
+      })
+  }
+
+  adicionar(nome: string) {
+    this.cidadeService.adicionar({ nome })
+      .then(cidade => {
+        alert(`Cidade "${cidade.nome}" adicionada com código ${cidade.id}!`);
+        this.consultar();
+      });
+  }
+
+  excluir(id: number) {
+    this.cidadeService.excluir(id)
+      .then(() => {
+        alert('Cidade excluída com sucesso!');
+        this.consultar();
+      });
+  }
+
+  atualizar(cidade: any) {
+    this.cidadeService.atualizar(cidade)
+      .then(() => {
+        alert('Cidade alterada com sucesso!');
+      });
+  }
+
+}
