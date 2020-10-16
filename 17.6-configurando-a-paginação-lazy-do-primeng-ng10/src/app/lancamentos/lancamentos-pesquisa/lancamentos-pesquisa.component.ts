4df55875e589047dc411416bdd65c87cac31243b
Fagner Lima <contato@fagnerlima.pro.br>
2020-10-16 07:22:04 -0300

    Atualiza a aula 17.6 para o ng10

---
+++ 17.6-configurando-a-paginação-lazy-do-primeng-ng10/src/app/lancamentos/lancamentos-pesquisa/lancamentos-pesquisa.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+
+import { LazyLoadEvent } from 'primeng/api';
+
+import { LancamentoService, LancamentoFiltro } from './../lancamento.service';
+
+@Component({
+  selector: 'app-lancamentos-pesquisa',
+  templateUrl: './lancamentos-pesquisa.component.html',
+  styleUrls: ['./lancamentos-pesquisa.component.css']
+})
+export class LancamentosPesquisaComponent implements OnInit {
+
+  totalRegistros = 0;
+  filtro = new LancamentoFiltro();
+  lancamentos = [];
+
+  constructor(private lancamentoService: LancamentoService) { }
+
+  ngOnInit() {
+  }
+
+  pesquisar(pagina = 0) {
+    this.filtro.pagina = pagina;
+
+    this.lancamentoService.pesquisar(this.filtro)
+      .then(resultado => {
+        this.totalRegistros = resultado.total;
+        this.lancamentos = resultado.lancamentos;
+      });
+  }
+
+  aoMudarPagina(event: LazyLoadEvent) {
+    const pagina = event.first / event.rows;
+    this.pesquisar(pagina);
+  }
+
+}
