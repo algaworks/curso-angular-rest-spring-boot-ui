9e64de8d6e6d6a7223162d6a2fbcb660e2acca63
Fagner Lima <contato@fagnerlima.pro.br>
2020-10-20 08:47:38 -0300

    Atualiza a aula 17.8 para o ng10

---
+++ 17.8-excluindo-lancamentos-e-o-decorador-viewchild-ng10/src/app/lancamentos/lancamentos-pesquisa/lancamentos-pesquisa.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+
+import { LazyLoadEvent } from 'primeng/api';
+import { Table } from 'primeng/table';
+
+import { LancamentoService, LancamentoFiltro } from './../lancamento.service';
+
+@Component({
+  selector: 'app-lancamentos-pesquisa',
+  templateUrl: './lancamentos-pesquisa.component.html',
+  styleUrls: ['./lancamentos-pesquisa.component.css']
+})
+export class LancamentosPesquisaComponent implements OnInit {
+
+  totalRegistros = 0;
+  filtro = new LancamentoFiltro();
+  lancamentos = [];
+  @ViewChild('tabela') grid: Table;
+
+  constructor(private lancamentoService: LancamentoService) { }
+
+  ngOnInit() {
+  }
+
+  pesquisar(pagina = 0) {
+    this.filtro.pagina = pagina;
+
+    this.lancamentoService.pesquisar(this.filtro)
+      .then(resultado => {
+        this.totalRegistros = resultado.total;
+        this.lancamentos = resultado.lancamentos;
+      });
+  }
+
+  aoMudarPagina(event: LazyLoadEvent) {
+    const pagina = event.first / event.rows;
+    this.pesquisar(pagina);
+  }
+
+  excluir(lancamento: any) {
+    this.lancamentoService.excluir(lancamento.codigo)
+      .then(() => {
+        this.grid.reset();
+      });
+  }
+
+}
---
+++ 17.8-excluindo-lancamentos-e-o-decorador-viewchild-ng10/src/app/pessoas/pessoa.service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+
+export class PessoaFiltro {
+  nome: string;
+  pagina = 0;
+  itensPorPagina = 5;
+}
+
+@Injectable()
+export class PessoaService {
+
+  pessoasUrl = 'http://localhost:8080/pessoas';
+
+  constructor(private http: HttpClient) { }
+
+  pesquisar(filtro: PessoaFiltro): Promise<any> {
+    const headers = new HttpHeaders()
+      .append('Authorization', 'Basic YWRtaW5AYWxnYW1vbmV5LmNvbTphZG1pbg==');
+    let params = new HttpParams()
+      .set('page', filtro.pagina.toString())
+      .set('size', filtro.itensPorPagina.toString());
+
+    if (filtro.nome) {
+      params = params.set('nome', filtro.nome);
+    }
+
+    return this.http.get(`${this.pessoasUrl}`, { headers, params })
+      .toPromise()
+      .then(response => {
+        const pessoas = response['content'];
+
+        const resultado = {
+          pessoas,
+          total: response['totalElements']
+        };
+
+        return resultado;
+      })
+  }
+
+  listarTodas(): Promise<any> {
+    const headers = new HttpHeaders()
+      .append('Authorization', 'Basic YWRtaW5AYWxnYW1vbmV5LmNvbTphZG1pbg==');
+
+    return this.http.get(this.pessoasUrl, { headers })
+      .toPromise()
+      .then(response => response['content']);
+  }
+
+}
