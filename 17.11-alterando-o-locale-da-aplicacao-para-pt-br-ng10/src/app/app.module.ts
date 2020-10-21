1e9eb8569e23d7476a600ca42d5d265af645d788
Fagner Lima <contato@fagnerlima.pro.br>
2020-10-21 10:02:09 -0300

    Atualiza a aula 17.11 para o ng10

---
+++ 17.11-alterando-o-locale-da-aplicacao-para-pt-br-ng10/src/app/app.module.ts
@@ -0,0 +1,49 @@
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, LOCALE_ID } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+
+import { MessageService, ConfirmationService } from 'primeng/api';
+import { ToastModule } from 'primeng/toast';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+
+import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { PessoasModule } from './pessoas/pessoas.module';
+import { LancamentosModule } from './lancamentos/lancamentos.module';
+import { PessoaService } from './pessoas/pessoa.service';
+import { LancamentoService } from './lancamentos/lancamento.service';
+
+registerLocaleData(localePt);
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+
+    ToastModule,
+    ConfirmDialogModule,
+
+    CoreModule,
+    LancamentosModule,
+    PessoasModule
+  ],
+  providers: [,
+    { provide: LOCALE_ID, useValue: 'pt-BR' },
+
+    MessageService,
+    ConfirmationService,
+
+    LancamentoService,
+    PessoaService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
