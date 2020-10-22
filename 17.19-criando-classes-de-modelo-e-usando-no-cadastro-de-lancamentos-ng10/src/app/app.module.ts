ee978b59b62e327689ee532e2ca4ad1875b8455d
Fagner Lima <contato@fagnerlima.pro.br>
2020-10-22 14:01:40 -0300

    Atualiza a aula 17.19 para o ng10

---
+++ 17.19-criando-classes-de-modelo-e-usando-no-cadastro-de-lancamentos-ng10/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { PessoasModule } from './pessoas/pessoas.module';
+import { LancamentosModule } from './lancamentos/lancamentos.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+
+    CoreModule,
+    LancamentosModule,
+    PessoasModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
