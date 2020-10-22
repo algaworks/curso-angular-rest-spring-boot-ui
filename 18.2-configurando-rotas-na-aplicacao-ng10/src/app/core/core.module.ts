e48ec480369edc90dc0ab0d756a54af46cdabd30
Fagner Lima <contato@fagnerlima.pro.br>
2020-10-22 14:39:40 -0300

    Atualiza a aula 18.2 para o ng10

---
+++ 18.2-configurando-rotas-na-aplicacao-ng10/src/app/core/core.module.ts
@@ -0,0 +1,42 @@
+import { NgModule, LOCALE_ID } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+
+import { MessageService, ConfirmationService } from 'primeng/api';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { ToastModule } from 'primeng/toast';
+
+import { ErrorHandlerService } from './error-handler.service';
+import { PessoaService } from './../pessoas/pessoa.service';
+import { LancamentoService } from './../lancamentos/lancamento.service';
+import { CategoriaService } from './../categorias/categoria.service';
+import { NavbarComponent } from './navbar/navbar.component';
+
+registerLocaleData(localePt);
+
+@NgModule({
+  imports: [
+    CommonModule,
+
+    ToastModule,
+    ConfirmDialogModule,
+  ],
+  declarations: [NavbarComponent],
+  exports: [
+    NavbarComponent,
+    ToastModule,
+    ConfirmDialogModule
+  ],
+  providers: [
+    LancamentoService,
+    PessoaService,
+    CategoriaService,
+    ErrorHandlerService,
+
+    ConfirmationService,
+    MessageService,
+    { provide: LOCALE_ID, useValue: 'pt-BR' }
+  ]
+})
+export class CoreModule { }
