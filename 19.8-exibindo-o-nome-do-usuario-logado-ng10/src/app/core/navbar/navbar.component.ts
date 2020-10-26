5f4637f1ccfe5bdc6ce9d84053513921a4842a25
Fagner Lima <contato@fagnerlima.pro.br>
2020-10-26 12:34:15 -0300

    Atualiza a aula 19.8 para o ng10

---
+++ 19.8-exibindo-o-nome-do-usuario-logado-ng10/src/app/core/navbar/navbar.component.ts
@@ -0,0 +1,18 @@
+import { AuthService } from './../../seguranca/auth.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+
+  exibindoMenu = false;
+
+  constructor(public auth: AuthService) { }
+
+  ngOnInit() {
+  }
+
+}
