9eff1795c924b79ab88382c621c32324949999da
Fagner Lima <contato@fagnerlima.pro.br>
2020-10-27 08:07:48 -0300

    Atualiza a aula 19.11 para o ng10

---
+++ 19.11-interceptando-chamadas-http-para-tratar-a-expiracao-do-access-token-ng10/src/app/core/navbar/navbar.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+import { AuthService } from './../../seguranca/auth.service';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+
+  exibindoMenu = false;
+
+  constructor(public auth: AuthService) { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ 19.11-interceptando-chamadas-http-para-tratar-a-expiracao-do-access-token-ng10/src/app/seguranca/money-http-interceptor.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
+
+import { Observable, from } from 'rxjs';
+import { mergeMap } from 'rxjs/operators';
+
+import { AuthService } from './auth.service';
+
+@Injectable()
+export class MoneyHttpInterceptor implements HttpInterceptor {
+
+  constructor(private auth: AuthService) {}
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    if (!req.url.includes('/oauth/token') && this.auth.isAccessTokenInvalido()) {
+      return from(this.auth.obterNovoAccessToken())
+        .pipe(
+          mergeMap(() => {
+            req = req.clone({
+              setHeaders: {
+                Authorization: `Bearer ${localStorage.getItem('token')}`
+              }
+            });
+
+            return next.handle(req);
+          })
+        );
+    }
+
+    return next.handle(req);
+  }
+}
