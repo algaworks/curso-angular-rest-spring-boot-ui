87ec8e267db07b9bc310eb343d0e61d1a7f4a4c3
Fagner Lima <contato@fagnerlima.pro.br>
2020-10-29 15:41:24 -0300

    Atualiza a aula 23.09 para o ng10

---
+++ 23.09-exibindo-o-pdf-para-o-usuario-ng10/src/app/pessoas/pessoa.service.ts
@@ -0,0 +1,78 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+
+import { environment } from './../../environments/environment';
+import { Pessoa } from './../core/model';
+
+export class PessoaFiltro {
+  nome: string;
+  pagina = 0;
+  itensPorPagina = 5;
+}
+
+@Injectable()
+export class PessoaService {
+
+  pessoasUrl: string;
+
+  constructor(private http: HttpClient) {
+    this.pessoasUrl = `${environment.apiUrl}/pessoas`;
+  }
+
+  pesquisar(filtro: PessoaFiltro): Promise<any> {
+    let params = new HttpParams()
+      .set('page', filtro.pagina.toString())
+      .set('size', filtro.itensPorPagina.toString());
+
+    if (filtro.nome) {
+      params = params.set('nome', filtro.nome);
+    }
+
+    return this.http.get(`${this.pessoasUrl}`, { params })
+      .toPromise()
+      .then(response => {
+        const pessoas = response['content'];
+
+        const resultado = {
+          pessoas,
+          total: response['totalElements']
+        };
+
+        return resultado;
+      })
+  }
+
+  listarTodas(): Promise<any> {
+    return this.http.get(this.pessoasUrl)
+      .toPromise()
+      .then(response => response['content']);
+  }
+
+  excluir(codigo: number): Promise<void> {
+    return this.http.delete(`${this.pessoasUrl}/${codigo}`)
+      .toPromise()
+      .then(() => null);
+  }
+
+  mudarStatus(codigo: number, ativo: boolean): Promise<void> {
+    return this.http.put(`${this.pessoasUrl}/${codigo}/ativo`, ativo)
+      .toPromise()
+      .then(() => null);
+  }
+
+  adicionar(pessoa: Pessoa): Promise<Pessoa> {
+    return this.http.post<Pessoa>(this.pessoasUrl, pessoa)
+      .toPromise();
+  }
+
+  atualizar(pessoa: Pessoa): Promise<Pessoa> {
+    return this.http.put<Pessoa>(`${this.pessoasUrl}/${pessoa.codigo}`, pessoa)
+      .toPromise();
+  }
+
+  buscarPorCodigo(codigo: number): Promise<Pessoa> {
+    return this.http.get<Pessoa>(`${this.pessoasUrl}/${codigo}`)
+      .toPromise();
+  }
+
+}
