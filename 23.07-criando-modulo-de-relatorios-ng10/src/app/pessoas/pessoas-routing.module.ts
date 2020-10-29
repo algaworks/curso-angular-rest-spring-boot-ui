8e450c060fb8c70a69efbe113459d93caf8ba27e
Fagner Lima <contato@fagnerlima.pro.br>
2020-10-29 15:22:37 -0300

    Atualiza a aula 23.07 para o ng10

---
+++ 23.07-criando-modulo-de-relatorios-ng10/src/app/pessoas/pessoas-routing.module.ts
@@ -0,0 +1,35 @@
+import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+
+import { AuthGuard } from './../seguranca/auth.guard';
+import { PessoaCadastroComponent } from './pessoa-cadastro/pessoa-cadastro.component';
+import { PessoasPesquisaComponent } from './pessoas-pesquisa/pessoas-pesquisa.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: PessoasPesquisaComponent,
+    canActivate: [AuthGuard],
+    data: { roles: ['ROLE_PESQUISAR_PESSOA'] }
+  },
+  {
+    path: 'nova',
+    component: PessoaCadastroComponent,
+    canActivate: [AuthGuard],
+    data: { roles: ['ROLE_CADASTRAR_PESSOA'] }
+  },
+  {
+    path: ':codigo',
+    component: PessoaCadastroComponent,
+    canActivate: [AuthGuard],
+    data: { roles: ['ROLE_CADASTRAR_PESSOA'] }
+  }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forChild(routes)
+  ],
+  exports: [RouterModule]
+})
+export class PessoasRoutingModule { }
