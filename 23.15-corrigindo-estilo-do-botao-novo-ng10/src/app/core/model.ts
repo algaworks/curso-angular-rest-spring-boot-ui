5dd637d7f354e6c7cdaf6cf8e859a6fc4359ec20
Fagner Lima <contato@fagnerlima.pro.br>
2020-10-30 07:19:07 -0300

    Atualiza aula 23.15 para o ng10

---
+++ 23.15-corrigindo-estilo-do-botao-novo-ng10/src/app/core/model.ts
@@ -0,0 +1,50 @@
+export class Endereco {
+  logradouro: string;
+  numero: string;
+  complemento: string;
+  bairro: string;
+  cep: string;
+  cidade: string;
+  estado: string;
+}
+
+export class Contato {
+  codigo: number;
+  nome: string;
+  email: string;
+  telefone: string;
+
+  constructor(codigo?: number,
+    nome?: string,
+    email?: string,
+    telefone?: string) {
+      this.codigo = codigo;
+      this.nome = nome;
+      this.email = email;
+      this.telefone = telefone;
+  }
+}
+
+export class Pessoa {
+  codigo: number;
+  nome: string;
+  endereco = new Endereco();
+  ativo = true;
+  contatos = new Array<Contato>();
+}
+
+export class Categoria {
+  codigo: number;
+}
+
+export class Lancamento {
+  codigo: number;
+  tipo = 'RECEITA';
+  descricao: string;
+  dataVencimento: Date;
+  dataPagamento: Date;
+  valor: number;
+  observacao: string;
+  pessoa = new Pessoa();
+  categoria = new Categoria();
+}
