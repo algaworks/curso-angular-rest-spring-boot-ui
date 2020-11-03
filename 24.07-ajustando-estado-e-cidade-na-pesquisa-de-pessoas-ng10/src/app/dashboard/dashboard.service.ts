1549ced80ae5430e068d69241c7879bd551aa8cc
Fagner Lima <contato@fagnerlima.pro.br>
2020-11-03 11:04:51 -0300

    Atualiza a aula 24.07 para o ng10

---
+++ 24.07-ajustando-estado-e-cidade-na-pesquisa-de-pessoas-ng10/src/app/dashboard/dashboard.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import * as moment from 'moment';
+
+import { environment } from './../../environments/environment';
+
+@Injectable()
+export class DashboardService {
+
+  lancamentosUrl: string;
+
+  constructor(private http: HttpClient) {
+    this.lancamentosUrl = `${environment.apiUrl}/lancamentos`;
+  }
+
+  lancamentosPorCategoria(): Promise<Array<any>> {
+    return this.http.get<any>(`${this.lancamentosUrl}/estatisticas/por-categoria`)
+      .toPromise();
+  }
+
+  lancamentosPorDia(): Promise<Array<any>> {
+    return this.http.get<Array<any>>(`${this.lancamentosUrl}/estatisticas/por-dia`)
+      .toPromise()
+      .then(response => {
+        this.converterStringsParaDatas(response);
+
+        return response;
+      });
+  }
+
+  private converterStringsParaDatas(dados: Array<any>) {
+    for (const dado of dados) {
+      dado.dia = moment(dado.dia, 'YYYY-MM-DD').toDate();
+    }
+  }
+}
