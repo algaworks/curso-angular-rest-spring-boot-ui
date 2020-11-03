3dafa5f3f51d14150b80fcc3cf82fd52db22d6db
Fagner Lima <contato@fagnerlima.pro.br>
2020-11-03 10:22:10 -0300

    Atualiza a aula 23.23 para o ng10

---
+++ 23.23-desabilitando-botao-salvar-no-upload-ng10/src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { NaoAutorizadoComponent } from './core/nao-autorizado.component';
+import { PaginaNaoEncontradaComponent } from './core/pagina-nao-encontrada.component';
+
+const routes: Routes = [
+  { path: 'lancamentos', loadChildren: () => import('app/lancamentos/lancamentos.module').then(m => m.LancamentosModule) },
+  { path: 'pessoas', loadChildren: () => import('app/pessoas/pessoas.module').then(m => m.PessoasModule) },
+  { path: 'dashboard', loadChildren: () => import('app/dashboard/dashboard.module').then(m => m.DashboardModule) },
+  { path: 'relatorios', loadChildren: () => import('app/relatorios/relatorios.module').then(m => m.RelatoriosModule) },
+
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: 'nao-autorizado', component: NaoAutorizadoComponent },
+  { path: 'pagina-nao-encontrada', component: PaginaNaoEncontradaComponent },
+  { path: '**', redirectTo: 'pagina-nao-encontrada' }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
