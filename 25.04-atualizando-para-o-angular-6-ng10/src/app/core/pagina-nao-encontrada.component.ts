5e9ccbd06c07ee5c453d979964c71f7a836ffa48
Fagner Lima <contato@fagnerlima.pro.br>
2020-11-03 11:07:26 -0300

    Atualiza a aula 25.04 para o ng10

---
+++ 25.04-atualizando-para-o-angular-6-ng10/src/app/core/pagina-nao-encontrada.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  template: `
+    <div class="container">
+      <h1 class="text-center">Página não encontrada</h1>
+    <div>
+  `,
+  styles: []
+})
+export class PaginaNaoEncontradaComponent {
+}
