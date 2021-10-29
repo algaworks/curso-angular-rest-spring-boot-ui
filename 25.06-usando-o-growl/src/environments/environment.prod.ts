52f47e43b093f24a5d722051c1a6385e71b116b6
Alex <alex.silva@algaworks.com.br>
2021-10-29 14:38:17 -0300

    Removido módulo 25

--- 25.06-usando-o-growl/src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'https://algamoney-api.herokuapp.com'
-};
--- 25.09-usando-a-classe-moneyhttp/src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'https://algamoney-api.herokuapp.com',
-
-  tokenWhitelistedDomains: [ /algamoney-api.herokuapp.com/ ],
-  tokenBlacklistedRoutes: [/\/oauth\/token/]
-};
