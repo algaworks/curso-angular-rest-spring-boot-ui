b5fb60e1cc8cc954c0ed71bfab9c466dc159a901
AlsGomes <als_08.net@hotmail.com>
2022-08-06 12:18:41 -0300

    Adicionado aula 10.11 com código-fonte atualizado para o Angular 14

---
+++ 10.11-criando-o-componente-de-barra-de-navegacao-ng14/src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { TableModule } from 'primeng/table';
+import { TabViewModule } from 'primeng/tabview';
+import { TooltipModule } from 'primeng/tooltip';
+
+import { AppComponent } from './app.component';
+import { LancamentosPesquisaComponent } from './lancamentos-pesquisa/lancamentos-pesquisa.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LancamentosPesquisaComponent,
+    NavbarComponent
+  ],
+  imports: [
+    BrowserModule,
+
+    ButtonModule,
+    InputTextModule,
+    TableModule,
+    TabViewModule,
+    TooltipModule
+    
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
