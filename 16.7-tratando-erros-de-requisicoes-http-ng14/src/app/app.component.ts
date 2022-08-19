a184ed913ea6d7c5e7cccbb037c5a1306dd75b94
AlsGomes <als_08.net@hotmail.com>
2022-08-18 23:17:51 -0300

    Adicionado aula 16.7 com código-fonte atualizado para o Angular 14

---
+++ 16.7-tratando-erros-de-requisicoes-http-ng14/src/app/app.component.ts
@@ -0,0 +1,57 @@
+import { CidadeService } from './cidade.service';
+import { Component, OnInit } from '@angular/core';
+
+interface Cidade {
+  id: number,
+  nome: string
+}
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  
+  cidades: Cidade[] = [];
+
+  constructor(private cidadeService: CidadeService) {}
+  
+  ngOnInit(): void {
+    this.consultar();
+  }
+  
+  consultar() {
+    this.cidadeService.consultar()
+      .then(dados => {
+        this.cidades = dados;
+      })
+  }
+
+  adicionar(nome: string) {
+    this.cidadeService.adicionar({ nome })
+      .then(cidade => {
+        alert(`Cidade "${cidade.nome}" adicionada com código ${cidade.id}!`);
+        this.consultar();
+      })
+  }
+
+  excluir(id: number) {
+    this.cidadeService.excluir(id)
+    .then(() => {
+      alert('Cidade excluída com sucesso!');
+      this.consultar();
+    });
+  }
+
+  atualizar(cidade: any) {
+    this.cidadeService.atualizar(cidade)
+      .then(() => {
+        alert('Cidade alterada com sucesso!');
+      })
+      .catch(erro => {
+        alert(erro);
+      });;
+  }
+  
+}
