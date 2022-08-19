1684621f159cbaf6713eb452a0756ffa4ae74d5c
AlsGomes <als_08.net@hotmail.com>
2022-08-18 21:47:49 -0300

    Adicionado aula 15.8 com código-fonte atualizado para o Angular 14

---
+++ 15.8-como-funciona-o-injetor-hierarquico-ng14/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { FuncionarioService } from './funcionario/funcionario.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+
+
+  funcionarios: any[] = [];
+
+  constructor(private funcionarioService: FuncionarioService) { }
+
+  ngOnInit() {
+    this.funcionarios = this.funcionarioService.consultar();
+  }
+}
---
+++ 15.8-como-funciona-o-injetor-hierarquico-ng14/src/app/log.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from "@angular/core";
+
+@Injectable()
+export class LogService {
+
+  log(msg: string) {
+    console.log(`LOG: ${msg}`);
+  }
+
+}
