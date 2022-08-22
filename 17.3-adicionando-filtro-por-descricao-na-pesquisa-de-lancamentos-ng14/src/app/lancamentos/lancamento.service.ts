f58e47d3db2e4c16a82e290a0930c43b566833d8
AlsGomes <als_08.net@hotmail.com>
2022-08-22 18:28:16 -0300

    Adicionado aula 17.3 com código-fonte atualizado para o Angular 14

---
+++ 17.3-adicionando-filtro-por-descricao-na-pesquisa-de-lancamentos-ng14/src/app/lancamentos/lancamento.service.ts
@@ -0,0 +1,31 @@
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+export interface LancamentoFiltro {
+  descricao: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LancamentoService {
+
+  lancamentosUrl = 'http://localhost:8080/lancamentos';
+
+  constructor(private http: HttpClient) { }
+
+  pesquisar(filtro: LancamentoFiltro): Promise<any> {
+    const headers = new HttpHeaders()
+      .append('Authorization', 'Basic YWRtaW5AYWxnYW1vbmV5LmNvbTphZG1pbg==');
+
+    let params = new HttpParams();
+
+    if (filtro.descricao) {
+      params = params.set('descricao', filtro.descricao);
+    }
+
+    return this.http.get(`${this.lancamentosUrl}?resumo`, { headers, params })
+      .toPromise()
+      .then((response: any) => response['content']);
+  }
+}
