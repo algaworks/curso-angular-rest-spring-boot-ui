7d6975e79fc3a911a7cf24892cbd0893784da90a
AlsGomes <als_08.net@hotmail.com>
2022-08-23 21:45:13 -0300

    Adicionado aula 17.5 com código-fonte atualizado para o Angular 14

---
+++ 17.5-implementando-a-paginacao-no-servico-de-lancamentos-ng14/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { LOCALE_ID, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppComponent } from './app.component';
+
+import { CoreModule } from './core/core.module';
+import { LancamentosModule } from './lancamentos/lancamentos.module';
+import { PessoasModule } from './pessoas/pessoas.module';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+
+    CoreModule,
+    LancamentosModule,
+    PessoasModule,
+  ],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'pt-BR' }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ 17.5-implementando-a-paginacao-no-servico-de-lancamentos-ng14/src/app/lancamentos/lancamento.service.ts
@@ -0,0 +1,58 @@
+import { DatePipe } from '@angular/common';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+export class LancamentoFiltro {
+  descricao?: string;
+  dataVencimentoInicio?: Date;
+  dataVencimentoFim?: Date;
+  pagina = 0;
+  itensPorPagina = 5;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LancamentoService {
+
+  lancamentosUrl = 'http://localhost:8080/lancamentos';
+
+  constructor(private http: HttpClient,
+    private datePipe: DatePipe) { }
+
+  pesquisar(filtro: LancamentoFiltro): Promise<any> {
+    const headers = new HttpHeaders()
+      .append('Authorization', 'Basic YWRtaW5AYWxnYW1vbmV5LmNvbTphZG1pbg==');
+
+    let params = new HttpParams()
+      .set('page', filtro.pagina)
+      .set('size', filtro.itensPorPagina);
+
+
+    if (filtro.descricao) {
+      params = params.set('descricao', filtro.descricao);
+    }
+
+    if (filtro.dataVencimentoInicio) {
+      params = params.set('dataVencimentoDe', this.datePipe.transform(filtro.dataVencimentoInicio, 'yyyy-MM-dd')!);
+    }
+
+    if (filtro.dataVencimentoFim) {
+      params = params.set('dataVencimentoAte', this.datePipe.transform(filtro.dataVencimentoFim, 'yyyy-MM-dd')!);
+    }
+
+    return this.http.get(`${this.lancamentosUrl}?resumo`, { headers, params })
+      .toPromise()
+      .then((response: any) => {
+        const lancamentos = response['content'];
+
+        const resultado = {
+          lancamentos,
+          total: response['totalElements']
+        };
+
+        return resultado;
+      });
+  }
+
+}
