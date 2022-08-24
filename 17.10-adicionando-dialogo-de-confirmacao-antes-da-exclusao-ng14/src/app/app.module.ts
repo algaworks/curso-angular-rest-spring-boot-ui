8b682c71134b42fe842a7e36f16383b5f7621acd
AlsGomes <als_08.net@hotmail.com>
2022-08-24 09:09:22 -0300

    Adicionado aula 17.10 com código-fonte atualizado para o Angular 14

---
+++ 17.10-adicionando-dialogo-de-confirmacao-antes-da-exclusao-ng14/src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { ConfirmationService, MessageService } from 'primeng/api';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { ToastModule } from 'primeng/toast';
+
+import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { LancamentosModule } from './lancamentos/lancamentos.module';
+import { PessoasModule } from './pessoas/pessoas.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+
+    CoreModule,
+    LancamentosModule,
+    PessoasModule,
+
+    ToastModule,
+    ConfirmDialogModule
+  ],
+  providers: [
+    MessageService,
+    ConfirmationService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
