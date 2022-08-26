9247907c152d0f3c90b3cccb26ada5856892d5a1
AlsGomes <als_08.net@hotmail.com>
2022-08-25 22:19:34 -0300

    Adicionado aula 17.13 com código-fonte atualizado para o Angular 14

---
+++ 17.13-desafio-implementando-a-exclusao-de-pessoas-ng14/src/app/core/error-handler.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { MessageService } from 'primeng/api';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ErrorHandlerService {
+
+  constructor(private messageService: MessageService) { }
+
+  handle(errorResponse: any) {
+    let msg: string;
+
+    if (typeof errorResponse === 'string') {
+      msg = errorResponse;
+    } else {
+      msg = 'Erro ao processar serviço remoto. Tente novamente.';
+      console.error('Ocorreu um erro', errorResponse);
+    }
+
+    this.messageService.add({ severity: 'error', detail: msg });
+  }
+}
