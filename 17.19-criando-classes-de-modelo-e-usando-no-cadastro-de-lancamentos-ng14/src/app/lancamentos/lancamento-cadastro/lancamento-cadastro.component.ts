8dc00368f8e0e8cad824bd7413597e12a98341c6
AlsGomes <als_08.net@hotmail.com>
2022-08-27 11:48:55 -0300

    Adicionado aula 17.19 com código-fonte atualizado para o Angular 14

---
+++ 17.19-criando-classes-de-modelo-e-usando-no-cadastro-de-lancamentos-ng14/src/app/lancamentos/lancamento-cadastro/lancamento-cadastro.component.ts
@@ -0,0 +1,61 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+import { Lancamento } from './../../core/model';
+import { ErrorHandlerService } from 'src/app/core/error-handler.service';
+import { PessoaService } from 'src/app/pessoas/pessoa.service';
+import { CategoriaService } from './../../categorias/categoria.service';
+
+@Component({
+  selector: 'app-lancamento-cadastro',
+  templateUrl: './lancamento-cadastro.component.html',
+  styleUrls: ['./lancamento-cadastro.component.css']
+})
+export class LancamentoCadastroComponent implements OnInit {
+
+  lancamento: Lancamento = new Lancamento();
+
+  categorias: any[] = [];
+  pessoas: any[] = []
+
+  tipos = [
+    { label: 'Receita', value: 'RECEITA' },
+    { label: 'Despesa', value: 'DESPESA' },
+  ];
+
+
+  constructor(
+    private categoriaService: CategoriaService,
+    private pessoaService: PessoaService,
+    private errorHandler: ErrorHandlerService
+  ) { }
+
+  ngOnInit(): void {
+    this.carregarCategorias()
+    this.carregarPessoas()
+  }
+
+  carregarCategorias() {
+    return this.categoriaService.listarTodas()
+      .then(categorias => {
+        this.categorias = categorias
+          .map((c: any) => ({ label: c.nome, value: c.codigo }));
+      })
+      .catch(erro => this.errorHandler.handle(erro));
+  }
+
+  carregarPessoas() {
+    this.pessoaService.listarTodas()
+      .then(pessoas => {
+        this.pessoas = pessoas
+          .map((p: any) => ({ label: p.nome, value: p.codigo }));
+      })
+      .catch(erro => this.errorHandler.handle(erro));
+  }
+
+  salvar(lancamentoForm: NgForm) {
+    console.log(this.lancamento);
+
+  }
+
+}
