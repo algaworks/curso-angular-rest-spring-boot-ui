361d28cec49ec6acad1453682e336e1d3d46258b
AlsGomes <als_08.net@hotmail.com>
2022-08-27 12:17:44 -0300

    Adicionado aula 17.20 com código-fonte atualizado para o Angular 14

---
+++ 17.20-implementando-o-servico-de-cadastro-de-lancamentos-ng14/src/app/pessoas/pessoa-cadastro/pessoa-cadastro.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pessoa-cadastro',
+  templateUrl: './pessoa-cadastro.component.html',
+  styleUrls: ['./pessoa-cadastro.component.css']
+})
+export class PessoaCadastroComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
