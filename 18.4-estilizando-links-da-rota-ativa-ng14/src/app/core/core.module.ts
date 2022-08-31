dc4b44fd739ea99c4fa306ad0cb1bd8636d6c911
AlsGomes <als_08.net@hotmail.com>
2022-08-30 21:50:17 -0300

    Adicionado aula 18.4 com código-fonte atualizado para o Angular 14

---
+++ 18.4-estilizando-links-da-rota-ativa-ng14/src/app/core/core.module.ts
@@ -0,0 +1,59 @@
+import { CommonModule, DatePipe, registerLocaleData } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import localePt from '@angular/common/locales/pt';
+
+import { NgModule } from '@angular/core';
+
+import { RouterModule } from '@angular/router';
+
+import { TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+import { ConfirmationService, MessageService } from 'primeng/api';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { ToastModule } from 'primeng/toast';
+
+import { ErrorHandlerService } from './error-handler.service';
+import { NavbarComponent } from './navbar/navbar.component';
+
+registerLocaleData(localePt, 'pt-BR');
+
+export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
+  return new TranslateHttpLoader(http);
+}
+
+@NgModule({
+  declarations: [
+    NavbarComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+
+    ToastModule,
+    ConfirmDialogModule,
+
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    }),
+  ],
+  exports: [
+    NavbarComponent,
+
+    ToastModule,
+    ConfirmDialogModule,
+  ],
+  providers: [
+    DatePipe,
+    ErrorHandlerService,
+
+    MessageService,
+    ConfirmationService,
+    TranslateService
+  ]
+})
+export class CoreModule { }
