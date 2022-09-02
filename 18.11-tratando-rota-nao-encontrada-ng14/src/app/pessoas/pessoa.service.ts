cb6a2fb46c6f1ca134ca241f66e2b22fe9961ad3
AlsGomes <als_08.net@hotmail.com>
2022-09-01 23:33:31 -0300

    Adicionado aula 18.11 com código-fonte atualizado para o Angular 14

---
+++ 18.11-tratando-rota-nao-encontrada-ng14/src/app/pessoas/pessoa.service.ts
@@ -0,0 +1,80 @@
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Pessoa } from '../core/model';
+
+export class PessoaFiltro {
+  nome?: string;
+  pagina: number = 0;
+  itensPorPagina: number = 5;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PessoaService {
+  pessoasUrl = 'http://localhost:8080/pessoas';
+
+  constructor(private http: HttpClient) { }
+
+  pesquisar(filtro: PessoaFiltro): Promise<any> {
+
+    const headers = new HttpHeaders()
+      .append('Authorization', 'Basic YWRtaW5AYWxnYW1vbmV5LmNvbTphZG1pbg==');
+
+    let params = new HttpParams()
+      .set('page', filtro.pagina)
+      .set('size', filtro.itensPorPagina);
+
+    if (filtro.nome) {
+      params = params.set('nome', filtro.nome);
+    }
+
+    return this.http.get(`${this.pessoasUrl}`, { headers, params })
+      .toPromise()
+      .then((response: any) => {
+        const pessoas = response['content'];
+
+        const resultado = {
+          pessoas,
+          total: response['totalElements']
+        };
+
+        return resultado;
+      });
+  }
+
+  listarTodas(): Promise<any> {
+    const headers = new HttpHeaders()
+      .append('Authorization', 'Basic YWRtaW5AYWxnYW1vbmV5LmNvbTphZG1pbg==');
+
+    return this.http.get(this.pessoasUrl, { headers })
+      .toPromise()
+      .then((response: any) => response['content']);
+  }
+
+  excluir(codigo: number): Promise<void> {
+    const headers = new HttpHeaders()
+      .append('Authorization', 'Basic YWRtaW5AYWxnYW1vbmV5LmNvbTphZG1pbg==');
+
+    return this.http.delete<void>(`${this.pessoasUrl}/${codigo}`, { headers }).toPromise();
+  }
+
+  mudarStatus(codigo: number, ativo: boolean): Promise<void> {
+    const headers = new HttpHeaders()
+      .append('Authorization', 'Basic YWRtaW5AYWxnYW1vbmV5LmNvbTphZG1pbg==')
+      .append('Content-Type', 'application/json');
+
+    return this.http.put<void>(`${this.pessoasUrl}/${codigo}/ativo`, ativo, { headers })
+      .toPromise();
+  }
+
+  adicionar(pessoa: Pessoa): Promise<Pessoa> {
+    const headers = new HttpHeaders()
+      .append('Authorization', 'Basic YWRtaW5AYWxnYW1vbmV5LmNvbTphZG1pbg==')
+      .append('Content-Type', 'application/json');
+
+    return this.http.post<Pessoa>(this.pessoasUrl, pessoa, { headers })
+      .toPromise();
+  }
+
+}
