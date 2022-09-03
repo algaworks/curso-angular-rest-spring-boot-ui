60a24634bc9f99670040e902f07cbdb017859a2e
AlsGomes <als_08.net@hotmail.com>
2022-09-03 12:34:23 -0300

    Adicionado aula 18.12 com código-fonte atualizado para o Angular 14

---
+++ 18.12-definindo-o-titulo-da-pagina-dinamicamente-ng14/src/app/pessoas/pessoa-cadastro/pessoa-cadastro.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+import { MessageService } from 'primeng/api';
+
+import { ErrorHandlerService } from 'src/app/core/error-handler.service';
+import { Pessoa } from 'src/app/core/model';
+import { PessoaService } from '../pessoa.service';
+
+@Component({
+  selector: 'app-pessoa-cadastro',
+  templateUrl: './pessoa-cadastro.component.html',
+  styleUrls: ['./pessoa-cadastro.component.css']
+})
+export class PessoaCadastroComponent implements OnInit {
+
+  pessoa = new Pessoa();
+
+  constructor(
+    private pessoaService: PessoaService,
+    private messageService: MessageService,
+    private errorHandler: ErrorHandlerService
+  ) { }
+
+  ngOnInit() {
+  }
+
+  salvar(form: NgForm) {
+    this.pessoaService.adicionar(this.pessoa)
+      .then(() => {
+        this.messageService.add({ severity: 'success', detail: 'Pessoa adicionada com sucesso!' });
+
+        form.reset();
+        this.pessoa = new Pessoa();
+      })
+      .catch(erro => this.errorHandler.handle(erro));
+  }
+
+}
