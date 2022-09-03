e8fc36094660574877b6808c781af86e591823f0
AlsGomes <als_08.net@hotmail.com>
2022-09-03 14:13:22 -0300

    Adicionado aula 18.15 com código-fonte atualizado para o Angular 14

---
+++ 18.15-desafio-roteamento-e-edicao-de-pessoas-ng14/src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { PrimeNGConfig } from 'primeng/api';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'algamoney-ui';
+
+  constructor(
+    private config: PrimeNGConfig,
+    private translateService: TranslateService
+  ) { }
+
+  ngOnInit() {
+    this.translateService.setDefaultLang('pt');
+    this.translateService.get('primeng')
+      .subscribe(res => this.config.setTranslation(res));
+  }
+}
