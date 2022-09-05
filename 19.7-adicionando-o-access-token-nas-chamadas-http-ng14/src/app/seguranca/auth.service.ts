fb5706251a0be9902523b16c4805dc671e173c01
AlsGomes <als_08.net@hotmail.com>
2022-09-05 11:22:38 -0300

    Adicionado aula 19.7 com código-fonte atualizado para o Angular 14

---
+++ 19.7-adicionando-o-access-token-nas-chamadas-http-ng14/src/app/seguranca/auth.service.ts
@@ -0,0 +1,58 @@
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  oauthTokenUrl = 'http://localhost:8080/oauth/token';
+  jwtPayload: any;
+
+  constructor(
+    private http: HttpClient,
+    private jwtHelper: JwtHelperService
+  ) {
+    this.carregarToken();
+  }
+
+  login(usuario: string, senha: string): Promise<void> {
+    const headers = new HttpHeaders()
+      .append('Content-Type', 'application/x-www-form-urlencoded')
+      .append('Authorization', 'Basic YW5ndWxhcjpAbmd1bEByMA==');
+
+    const body = `username=${usuario}&password=${senha}&grant_type=password`;
+
+    return this.http.post(this.oauthTokenUrl, body, { headers })
+      .toPromise()
+      .then((response: any) => {
+        this.armazenarToken(response['access_token']);
+      })
+      .catch(response => {
+        if (response.status === 400) {
+          if (response.error.error === 'invalid_grant') {
+            return Promise.reject('Usuário ou senha inválida!');
+          }
+        }
+
+        return Promise.reject(response);
+      });
+  }
+
+  public armazenarToken(token: string) {
+    this.jwtPayload = this.jwtHelper.decodeToken(token);
+    console.log(this.jwtPayload);
+
+    localStorage.setItem('token', token);
+  }
+
+  public carregarToken() {
+    const token = localStorage.getItem('token');
+
+    if (token) {
+      this.armazenarToken(token);
+    }
+  }
+
+}
