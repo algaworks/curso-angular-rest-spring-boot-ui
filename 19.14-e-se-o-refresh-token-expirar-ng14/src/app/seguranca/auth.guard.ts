402d7963c40eeba65472ad0125d65d999d338958
AlsGomes <als_08.net@hotmail.com>
2022-09-06 12:21:29 -0300

    Adicionado aula 19.14 com código-fonte atualizado para o Angular 14

---
+++ 19.14-e-se-o-refresh-token-expirar-ng14/src/app/seguranca/auth.guard.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from './auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  constructor(
+    private auth: AuthService,
+    private router: Router
+  ) { }
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+
+    if (next.data.roles && !this.auth.temQualquerPermissao(next.data.roles)) {
+      this.router.navigate(['/nao-autorizado']);
+      return false;
+    }
+
+    return true;
+
+  }
+
+}
