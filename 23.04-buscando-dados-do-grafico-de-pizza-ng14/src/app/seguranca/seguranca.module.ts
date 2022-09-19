bece285425c194c439b747bb3fb4e36fc65fa90b
AlsGomes <als_08.net@hotmail.com>
2022-09-19 15:36:54 -0300

    Adicionado aula 23.04 com código-fonte atualizado para o Angular 14

---
+++ 23.04-buscando-dados-do-grafico-de-pizza-ng14/src/app/seguranca/seguranca.module.ts
@@ -0,0 +1,50 @@
+import { environment } from './../../environments/environment';
+import { AuthGuard } from './auth.guard';
+import { CommonModule } from '@angular/common';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { JwtHelperService, JwtModule } from '@auth0/angular-jwt';
+
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { MoneyHttpInterceptor } from './money-http-interceptor';
+import { SegurancaRoutingModule } from './seguranca-routing.module';
+
+export function tokenGetter(): string {
+  return localStorage.getItem('token')!;
+}
+@NgModule({
+  declarations: [
+    LoginFormComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+
+    JwtModule.forRoot({
+      config: {
+        tokenGetter,
+        allowedDomains: environment.tokenAllowedDomains,
+        disallowedRoutes: environment.tokenDisallowedRoutes
+      }
+    }),
+
+    InputTextModule,
+    ButtonModule,
+
+    SegurancaRoutingModule,
+  ],
+  providers: [
+    JwtHelperService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: MoneyHttpInterceptor,
+      multi: true
+    },
+    AuthGuard
+  ]
+})
+export class SegurancaModule { }
