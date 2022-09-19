42a1e0f2df88358b0c032400b4d7b5c6fd71d7ea
AlsGomes <als_08.net@hotmail.com>
2022-09-19 11:30:14 -0300

    Adicionado aula 23.02 com código-fonte atualizado para o Angular 14

---
+++ 23.02-plotando-graficos-com-dados-estaticos-ng14/src/app/core/error-handler.service.ts
@@ -0,0 +1,47 @@
+import { HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { MessageService } from 'primeng/api';
+import { NotAuthenticatedError } from './../seguranca/money-http-interceptor';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ErrorHandlerService {
+
+  constructor(
+    private messageService: MessageService,
+    private router: Router) { }
+
+  handle(errorResponse: any) {
+    let msg: string;
+
+    if (typeof errorResponse === 'string') {
+      msg = errorResponse;
+    } else if (errorResponse instanceof NotAuthenticatedError) {
+      console.log('erro refresh');
+
+      msg = 'Sua sessão expirou!';
+      this.router.navigate(['/login']);
+
+    } else if (errorResponse instanceof HttpErrorResponse
+      && errorResponse.status >= 400 && errorResponse.status <= 499) {
+      msg = 'Ocorreu um erro ao processar a sua solicitação';
+
+      if (errorResponse.status === 403) {
+        msg = 'Você não tem permissão para executar esta ação';
+      }
+
+      try {
+        msg = errorResponse.error[0].mensagemUsuario;
+      } catch (e) { }
+
+      console.error('Ocorreu um erro', errorResponse);
+    } else {
+      msg = 'Erro ao processar serviço remoto. Tente novamente.';
+      console.error('Ocorreu um erro', errorResponse);
+    }
+
+    this.messageService.add({ severity: 'error', detail: msg });
+  }
+}
