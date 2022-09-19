4bcef05ed21997552dc93b8a332d4949836df06e
AlsGomes <als_08.net@hotmail.com>
2022-09-19 15:45:49 -0300

    Adicionado aula 23.05 com código-fonte atualizado para o Angular 14

---
+++ 23.05-buscando-dados-do-grafico-de-linhas-ng14/src/app/dashboard/dashboard.module.ts
@@ -0,0 +1,23 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { ChartModule } from 'primeng/chart';
+import { PanelModule } from 'primeng/panel';
+
+import { SharedModule } from './../shared/shared.module';
+import { DashboardRoutingModule } from './dashboard-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+@NgModule({
+  declarations: [DashboardComponent],
+  imports: [
+    CommonModule,
+
+    PanelModule,
+    ChartModule,
+
+    SharedModule,
+    DashboardRoutingModule
+  ]
+})
+export class DashboardModule { }
