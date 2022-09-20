23478722f7632fdee048ae094c10eeea687759fa
AlsGomes <als_08.net@hotmail.com>
2022-09-20 14:01:09 -0300

    Adicionado aula 23.09 com código-fonte atualizado para o Angular 14

---
+++ 23.09-exibindo-o-pdf-para-o-usuario-ng14/src/app/core/core.module.ts
@@ -0,0 +1,66 @@
+import { NaoAutorizadoComponent } from './nao-autorizado.component';
+import { CommonModule, DatePipe, registerLocaleData } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import localePt from '@angular/common/locales/pt';
+
+import { NgModule } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+
+import { TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+import { ConfirmationService, MessageService } from 'primeng/api';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { ToastModule } from 'primeng/toast';
+
+import { AuthService } from './../seguranca/auth.service';
+import { ErrorHandlerService } from './error-handler.service';
+import { NavbarComponent } from './navbar/navbar.component';
+import { PaginaNaoEncontradaComponent } from './pagina-nao-encontrada.component';
+
+registerLocaleData(localePt, 'pt-BR');
+
+export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
+  return new TranslateHttpLoader(http);
+}
+
+@NgModule({
+  declarations: [
+    NavbarComponent,
+    PaginaNaoEncontradaComponent,
+    NaoAutorizadoComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+
+    ToastModule,
+    ConfirmDialogModule,
+
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    }),
+  ],
+  exports: [
+    NavbarComponent,
+
+    ToastModule,
+    ConfirmDialogModule,
+  ],
+  providers: [
+    DatePipe,
+    ErrorHandlerService,
+    AuthService,
+
+    MessageService,
+    ConfirmationService,
+    Title,
+    TranslateService,
+  ]
+})
+export class CoreModule { }
